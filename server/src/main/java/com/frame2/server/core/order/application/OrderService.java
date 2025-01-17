4e0e86bb1511ecc49ba650a679bff5b813e1b396
kiteof_park <kiteof_park@naver.com>
2025-01-17 20:21:14 +0900

    feat: 주문/주문 상세 생성 기능 구현

--- server/src/main/java/com/frame2/server/core/order/application/OrderService.java
+++ server/src/main/java/com/frame2/server/core/order/application/OrderService.java
@@ -1,6 +1,8 @@
 package com.frame2.server.core.order.application;
 
+import com.frame2.server.core.order.payload.request.OrderCreateRequest;
+
 public interface OrderService {
 
-
+    public Long createOrder(OrderCreateRequest request);
 }
--- server/src/main/java/com/frame2/server/core/order/application/OrderServiceImpl.java
+++ server/src/main/java/com/frame2/server/core/order/application/OrderServiceImpl.java
@@ -1,7 +1,53 @@
 package com.frame2.server.core.order.application;
 
+import com.frame2.server.core.order.domain.Order;
+import com.frame2.server.core.order.domain.OrderDetail;
+import com.frame2.server.core.order.infrastructure.OrderDetailRepository;
+import com.frame2.server.core.order.infrastructure.OrderRepository;
+import com.frame2.server.core.order.payload.request.OrderCreateRequest;
+import com.frame2.server.core.product.domain.SaleProduct;
+import com.frame2.server.core.product.infrastructure.ProductRepository;
+import com.frame2.server.core.product.infrastructure.SaleProductRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-    
-    // 주문 생성
-    public void createOrder() {}
+
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final SaleProductRepository saleProductRepository;
+
+    @Transactional
+    public Long createOrder(OrderCreateRequest request) {
+        
+        // 주문 생성
+        Order order = request.toEntity();
+        orderRepository.save(order);
+        
+        // 주문 상세 생성
+        List<OrderDetail> orderDetails = request.orderDetails().stream()
+                .map(orderDetail -> {
+                    // 판매상품 조회
+                    SaleProduct saleProduct = saleProductRepository.findById(orderDetail.saleProductId())
+                            .orElseThrow(()-> new IllegalArgumentException("판매상품 ID가 유효하지 않습니다."));
+                    
+                    // 주문 상세 생성
+                    return OrderDetail.builder()
+                            .order(order)
+                            .saleProduct(saleProduct)
+                            .quantity(orderDetail.quantity())
+                            .price(saleProduct.getProduct().getPrice())
+                            .build();
+                })
+                .peek(orderDetail -> order.getOrderDetails().add(orderDetail))
+                .toList();
+
+        // 주문 번호 반환
+        return order.getId();
+    }
 }
