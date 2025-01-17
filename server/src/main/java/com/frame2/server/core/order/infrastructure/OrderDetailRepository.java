f74ba2a04be0fc4635b1cb40dad38d66aa452697
kiteof_park <kiteof_park@naver.com>
2025-01-17 20:20:40 +0900

    feat: Order, OrderDetail 레포지토리 구현

--- server/src/main/java/com/frame2/server/core/order/infrastructure/OrderDetailRepository.java
+++ server/src/main/java/com/frame2/server/core/order/infrastructure/OrderDetailRepository.java
@@ -1,11 +1,9 @@
 package com.frame2.server.core.order.infrastructure;
 
-import com.frame2.server.core.order.domain.Order;
-import org.hibernate.validator.constraints.pl.REGON;
+import com.frame2.server.core.order.domain.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
 }
