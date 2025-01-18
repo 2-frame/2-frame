62c2607d6bcf9ced7461c80701635202cc893228
heymeworld <com01@knou.ac.kr>
2025-01-19 02:56:47 +0900

    feat: 장바구니 상품 추가 기능 구현
    1. 회원은 장바구니 상품에 판매상품을 담을 수 있다.

--- server/src/main/java/com/frame2/server/core/cart/api/CartApiSpec.java
+++ server/src/main/java/com/frame2/server/core/cart/api/CartApiSpec.java
@@ -1,8 +1,10 @@
 package com.frame2.server.core.cart.api;
 
+import com.frame2.server.core.cart.payload.request.CartItemRequest;
 import com.frame2.server.core.cart.payload.response.CartItemListResponse;
 import com.frame2.server.core.support.annotations.Auth;
 import com.frame2.server.core.support.request.User;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
@@ -10,5 +12,9 @@ import java.util.List;
 @Tag(name = "장바구니 API")
 public interface CartApiSpec {
 
+    @Operation(summary = "장바구니 상품 조회")
     List<CartItemListResponse> getCartItems(User user);
+
+    @Operation(summary = "장바구니 상품 추가")
+    void addCartItem(CartItemRequest cartItemRequest);
 }
