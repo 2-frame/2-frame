74bcd1c428cadfd0ee70e007908003eeeb668bbf
devyoon <wideskyinme@gmail.com>
2025-01-20 08:40:51 +0900

    refactor: 불필요한 import제거

--- server/src/main/java/com/frame2/server/core/product/infrastructure/ProductRepository.java
+++ server/src/main/java/com/frame2/server/core/product/infrastructure/ProductRepository.java
@@ -5,7 +5,6 @@ import com.frame2.server.core.support.exception.DomainException;
 import com.frame2.server.core.support.exception.ExceptionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
