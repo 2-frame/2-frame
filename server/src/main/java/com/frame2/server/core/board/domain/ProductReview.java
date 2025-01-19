c6405c4b41a4bdb02d00da0505b0ecbe65a5d330
jaeho <jaho321@naver.com>
2025-01-20 06:02:38 +0900

    feat:상품 리뷰 엔티티 수정

--- server/src/main/java/com/frame2/server/core/board/domain/ProductReview.java
+++ server/src/main/java/com/frame2/server/core/board/domain/ProductReview.java
@@ -5,12 +5,12 @@ import com.frame2.server.core.order.domain.OrderDetail;
 import com.frame2.server.core.product.domain.SaleProduct;
 import com.frame2.server.core.support.entity.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Getter
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor
 @Table(name = "product_reviews")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductReview extends BaseEntity {
@@ -36,4 +36,11 @@ public class ProductReview extends BaseEntity {
     private String contents;
 
     private String image;
+
+    public ProductReview updateProductReview(ProductReview newProductReview){
+        this.rating = newProductReview.getRating();
+        this.contents = newProductReview.getContents();
+        this.image = newProductReview.getImage();
+        return this;
+    }
 }
