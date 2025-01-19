5285a1fd56ba17c81ea741e945a6682298d98030
jaeho <jaho321@naver.com>
2025-01-20 04:08:10 +0900

    feat:상품 리뷰 에러코드 및 레포지토리 생성

---
+++ server/src/main/java/com/frame2/server/core/board/application/ProductReviewService.java
@@ -0,0 +1,9 @@
+package com.frame2.server.core.board.application;
+
+import com.frame2.server.core.board.domain.ProductReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductReviewService {
+
+
+}
