40a9dc7d6ddfe814b9bb7221b3aea8e41561c4ed
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-01-19 22:35:30 +0900

    feat: 인증 코드 중에서 가장 마지막에 만료되는 코드를 조회하도록 구현

--- server/src/test/java/com/frame2/server/fixture/MemberFixture.java
+++ server/src/test/java/com/frame2/server/fixture/MemberFixture.java
@@ -4,6 +4,10 @@ import com.frame2.server.core.member.domain.Member;
 
 public class MemberFixture {
 
+    public static Member create() {
+        return create("test@email.com");
+    }
+
     public static Member create(String email) {
         return Member.builder()
                 .email(email)
