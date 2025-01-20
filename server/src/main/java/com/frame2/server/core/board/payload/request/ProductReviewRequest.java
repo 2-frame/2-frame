fdffc43c5ff4dd2ff859f9039b17d760e53686ed
jaho96 <jaho321@naver.com>
2025-01-20 21:34:42 +0900

    feat: 상품 리뷰 request 수정

--- server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewRequest.java
+++ server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewRequest.java
@@ -1,25 +1,18 @@
 package com.frame2.server.core.board.payload.request;
 
-import com.frame2.server.core.board.domain.ProductQnA;
 import com.frame2.server.core.board.domain.ProductReview;
 import com.frame2.server.core.member.domain.Member;
 import com.frame2.server.core.order.domain.OrderDetail;
 import com.frame2.server.core.product.domain.SaleProduct;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 
 public record ProductReviewRequest(
-        Long id,
-        SaleProduct saleProduct,
-        Member member,
-        OrderDetail orderDetail,
+        Long saleProductId,
+        Long orderDetailId,
         int rating,
         String contents,
         String image
 ) {
-    public ProductReview toEntity(){
+    public ProductReview toEntity(Member member, SaleProduct saleProduct, OrderDetail orderDetail) {
         return ProductReview.builder()
                 .saleProduct(saleProduct)
                 .member(member)
