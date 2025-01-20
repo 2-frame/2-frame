654ed19c4a2677ccb7fc009e196b8fe4e16b1d97
jaho96 <jaho321@naver.com>
2025-01-20 21:37:07 +0900

    feat: 상품 리뷰 api 수정

--- server/src/main/java/com/frame2/server/core/board/api/ProductReviewApi.java
+++ server/src/main/java/com/frame2/server/core/board/api/ProductReviewApi.java
@@ -1,11 +1,12 @@
 package com.frame2.server.core.board.api;
 
-import brave.Response;
 import com.frame2.server.core.board.application.ProductReviewService;
 import com.frame2.server.core.board.payload.request.ProductReviewModifyRequest;
 import com.frame2.server.core.board.payload.request.ProductReviewRequest;
-import com.frame2.server.core.board.payload.response.ProductQnAListResponse;
 import com.frame2.server.core.board.payload.response.ProductReviewResponse;
+import com.frame2.server.core.support.annotations.Auth;
+import com.frame2.server.core.support.annotations.MemberOnly;
+import com.frame2.server.core.support.request.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,29 +15,35 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/review")
-public class ProductReviewApi implements ProductReviewApiSpec{
+@RequestMapping("/reviews")
+public class ProductReviewApi implements ProductReviewApiSpec {
 
     private final ProductReviewService productReviewService;
 
+    // create
     @Override
-    @PostMapping("/add")
-    public void createProductReview(ProductReviewRequest productReviewRequest) {
-        productReviewService.productReviewCreate(productReviewRequest);
+//    @MemberOnly
+    @PostMapping
+    public void createProductReview(ProductReviewRequest productReviewRequest, @Auth User user) {
+        productReviewService.productReviewCreate(productReviewRequest, user.id());
     }
 
+    // update
     @Override
-    @PatchMapping("/{productReviewId")
+    @MemberOnly
+    @PatchMapping("/{productReviewId}")
     public void update(@RequestBody ProductReviewModifyRequest ProductReviewModifyRequest) {
         productReviewService.productReviewModify(ProductReviewModifyRequest);
     }
 
     @Override
+    @MemberOnly
     @DeleteMapping("/{productReviewId}")
     public void delete(@PathVariable("productReviewId") Long id) {
         productReviewService.remove(id);
     }
 
+    // 단건 조회
     @Override
     @GetMapping("/{productReviewId}")
     public ResponseEntity<ProductReviewResponse> getProductReview(@PathVariable("productReviewId") Long id) {
@@ -44,6 +51,7 @@ public class ProductReviewApi implements ProductReviewApiSpec{
         return ResponseEntity.ok().body(productReviewResponse);
     }
 
+    // 전체 조회
     @Override
     @GetMapping
     public ResponseEntity<List<ProductReviewResponse>> getProductReviewList() {
--- server/src/main/java/com/frame2/server/core/board/api/ProductReviewApiSpec.java
+++ server/src/main/java/com/frame2/server/core/board/api/ProductReviewApiSpec.java
@@ -3,26 +3,25 @@ package com.frame2.server.core.board.api;
 import com.frame2.server.core.board.payload.request.ProductReviewModifyRequest;
 import com.frame2.server.core.board.payload.request.ProductReviewRequest;
 import com.frame2.server.core.board.payload.response.ProductReviewResponse;
+import com.frame2.server.core.support.request.User;
 import jdk.jfr.Category;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @Category("상품 리뷰 api")
 public interface ProductReviewApiSpec {
     // 생성
-    void createProductReview(@RequestBody ProductReviewRequest productReviewRequest);
+    void createProductReview(ProductReviewRequest productReviewRequest, User user);
 
     // 수정
-    void update(@RequestBody ProductReviewModifyRequest ProductReviewModifyRequest);
+    void update(ProductReviewModifyRequest ProductReviewModifyRequest);
 
     // 식제
-    void delete(@PathVariable Long id);
+    void delete(Long id);
 
     // 단건 검색
-    ResponseEntity<ProductReviewResponse> getProductReview(@PathVariable Long id);
+    ResponseEntity<ProductReviewResponse> getProductReview(Long id);
 
     // 전체 검색
     ResponseEntity<List<ProductReviewResponse>> getProductReviewList();
