120de9d0f745c276d35621c97b4c89e73a62d628
jaho96 <jaho321@naver.com>
2025-01-22 05:49:11 +0900

    refactor:상품 리뷰 레포지토리 - 삭제 여부에 따른 조회 추가, 코드 리뷰 반영 1차

--- server/src/main/java/com/frame2/server/core/board/infrastructure/ProductReviewRepository.java
+++ server/src/main/java/com/frame2/server/core/board/infrastructure/ProductReviewRepository.java
@@ -4,11 +4,23 @@ import com.frame2.server.core.board.domain.ProductReview;
 import com.frame2.server.core.support.exception.DomainException;
 import com.frame2.server.core.support.exception.ExceptionType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
 
     default ProductReview findOne(Long id) {
-        return findById(id)
+        ProductReview productReview = findById(id)
                 .orElseThrow(() -> new DomainException(ExceptionType.PRODUCT_REVIEW_NOT_FOUND));
+        if (productReview.isDeleteStatus()) {
+            throw new DomainException(ExceptionType.PRODUCT_REVIEW_NOT_FOUND);
+        }
+        return productReview;
     }
+
+    // deleteStatus가 false이고, saleProduct의 id가 주어진 값인 데이터를 조회
+    @Query("SELECT pr FROM ProductReview pr WHERE pr.deleteStatus = false AND pr.saleProduct.id = :saleProductId")
+    List<ProductReview> findAllBySaleProduct_Id(@Param("saleProductId") Long saleProductId);
 }
