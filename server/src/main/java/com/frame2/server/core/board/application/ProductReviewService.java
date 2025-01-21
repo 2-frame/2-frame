1dbff0607c264814a63edb99af7775ddc056beb7
jaho96 <jaho321@naver.com>
2025-01-21 14:21:59 +0900

    feat: 상품리뷰 service 추가

---
+++ server/src/main/java/com/frame2/server/core/board/application/ProductReviewService.java
@@ -0,0 +1,25 @@
+package com.frame2.server.core.board.application;
+
+import com.frame2.server.core.board.domain.ProductReview;
+import com.frame2.server.core.board.payload.request.ProductReviewModifyRequest;
+import com.frame2.server.core.board.payload.request.ProductReviewRequest;
+import com.frame2.server.core.board.payload.response.ProductReviewResponse;
+
+import java.util.List;
+
+public interface ProductReviewService {
+    // 생성
+    ProductReview productReviewCreate(ProductReviewRequest request, Long memberId);
+
+    // 수정
+    ProductReview productReviewModify(ProductReviewModifyRequest request);
+
+    // 삭제
+    void remove(Long id);
+
+    // 단건 조회
+    ProductReviewResponse getProductReview(Long id);
+
+    // 전체 조회
+    List<ProductReviewResponse> getProductReviewList();
+}
