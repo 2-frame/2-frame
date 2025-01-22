d029f2920bc088f7d9a614332942dadea2e58180
StarlitDreamerGalaxyWanderer <ghddlfkdn@gmail.com>
2025-01-22 20:09:13 +0900

    feat: 클릭할 경우 툴팁이 표시되는 기능 구현

--- front/src/components/FloatingButton/FloatingButton.tsx
+++ front/src/components/FloatingButton/FloatingButton.tsx
@@ -1,17 +1,20 @@
 import {Button, Tooltip} from "@heroui/react";
 import ChatRoom from "../ChatRoom/ChatRoom.tsx";
+import {useState} from "react";
 
 const FloatingButton = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="fixed bottom-[20px] right-[20px]">
       <Tooltip
-        isOpen={true}
+        isOpen={isOpen}
         content={<ChatRoom/>}
         classNames={{
           content: ["p-0 shadow-xl", "text-black bg-none"],
         }}
       >
-        <Button>AI에게 물어보기</Button>
+        <Button onPress={() => setOpen(!isOpen)}>AI에게 물어보기</Button>
       </Tooltip>
     </div>
   )
