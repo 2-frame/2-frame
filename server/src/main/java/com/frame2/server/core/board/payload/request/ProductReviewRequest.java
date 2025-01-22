25e1f11bde88b693cd2d2052e26ba183d171ced3
jaho96 <jaho321@naver.com>
2025-01-22 14:14:13 +0900

    refactor: 상품 리뷰 request dto 검증 어노테이션 추가

--- server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewRequest.java
+++ server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewRequest.java
@@ -1,15 +1,30 @@
 package com.frame2.server.core.board.payload.request;
 
+import brave.internal.Nullable;
 import com.frame2.server.core.board.domain.ProductReview;
 import com.frame2.server.core.member.domain.Member;
 import com.frame2.server.core.order.domain.OrderDetail;
 import com.frame2.server.core.product.domain.SaleProduct;
+import jakarta.validation.constraints.*;
 
 public record ProductReviewRequest(
+
+        @NotNull
         Long saleProductId,
+
+        @NotNull
         Long orderDetailId,
+
+        @Min(1)
+        @Max(5)
+        @NotNull
         int rating,
+
+        @NotBlank
+        @Size(min = 10, max = 1000, message = "리뷰는 최소 10자에서 1000자까지 가능합니다.")
         String contents,
+
+        @Nullable
         String image
 ) {
     public ProductReview toEntity(Member member, SaleProduct saleProduct, OrderDetail orderDetail) {
