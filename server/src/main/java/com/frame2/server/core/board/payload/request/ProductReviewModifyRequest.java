912071d9dac90cc4d1d5156ef05f3b656dc3ce0d
jaho96 <jaho321@naver.com>
2025-01-22 14:58:16 +0900

    refactor: 상품 리뷰 request Dto 수정

--- server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewModifyRequest.java
+++ server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewModifyRequest.java
@@ -15,7 +15,7 @@ public record ProductReviewModifyRequest(
         @NotNull(message = "리뷰 평점은 1~5점까지 가능합니다.")
         int rating,
 
-        @NotBlank
+        @NotBlank(message = "내용은 필수 입력 사항 입니다.")
         @Size(min = 10, max = 1000, message = "리뷰는 최소 10자에서 1000자까지 가능합니다.")
         String contents,
 
--- server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewRequest.java
+++ server/src/main/java/com/frame2/server/core/board/payload/request/ProductReviewRequest.java
@@ -9,18 +9,18 @@ import jakarta.validation.constraints.*;
 
 public record ProductReviewRequest(
 
-        @NotNull
+        @NotNull(message = "판매 상품 id값이 담기지 않았습니다.")
         Long saleProductId,
 
-        @NotNull
+        @NotNull(message = "주문 상세 id값이 담기지 않았습니다.")
         Long orderDetailId,
 
         @Min(1)
         @Max(5)
-        @NotNull
+        @NotNull(message = "리뷰 평점은 1~5점까지 가능합니다.")
         int rating,
 
-        @NotBlank
+        @NotBlank(message = "내용은 필수 입력 사항 입니다.")
         @Size(min = 10, max = 1000, message = "리뷰는 최소 10자에서 1000자까지 가능합니다.")
         String contents,
 
