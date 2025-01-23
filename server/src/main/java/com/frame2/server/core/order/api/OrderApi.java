b45fe2fd22767a36943525a5c3601f870a3a07de
kiteof_park <kiteof_park@naver.com>
2025-01-23 18:35:50 +0900

    refactor: OrderAPi에 @Auth, User 추가

--- server/src/main/java/com/frame2/server/core/order/api/OrderApi.java
+++ server/src/main/java/com/frame2/server/core/order/api/OrderApi.java
@@ -4,6 +4,8 @@ import com.frame2.server.core.order.application.OrderService;
 import com.frame2.server.core.order.payload.request.OrderCreateRequest;
 import com.frame2.server.core.order.payload.response.OrderDetailResponse;
 import com.frame2.server.core.order.payload.response.OrderResponse;
+import com.frame2.server.core.support.annotations.Auth;
+import com.frame2.server.core.support.request.User;
 import com.frame2.server.core.support.annotations.MemberOnly;
 import com.frame2.server.core.support.response.IdResponse;
 import jakarta.validation.Valid;
@@ -28,58 +30,58 @@ public class OrderApi implements OrderApiSpec{
     @Override
     @MemberOnly
     @PostMapping
-    public IdResponse createOrder(@RequestBody @Valid OrderCreateRequest request) {
-        return orderService.createOrder(request);
+    public IdResponse createOrder(@Auth User user, @RequestBody @Valid OrderCreateRequest request) {
+        return orderService.createOrder(user.id() ,request);
     }
     
     // 주문 단건 조회
     @Override
     @MemberOnly
     @GetMapping("/{orderId}")
-    public OrderResponse getOrder(@PathVariable Long orderId) {
-        return orderService.getOrder(orderId);
+    public OrderResponse getOrder(@Auth User user, @PathVariable Long orderId) {
+        return orderService.getOrder(user.id(), orderId);
     }
     
     // 주문 전체 조회
     @Override
     @MemberOnly
-    @GetMapping("/members/{memberId}")
-    public PagedModel<OrderResponse> getOrders(@PathVariable Long memberId,
+    @GetMapping("/members")
+    public PagedModel<OrderResponse> getOrders(@Auth User user,
                                                @RequestParam(name = "page", defaultValue = "1") int page,
                                                @RequestParam(name = "size", defaultValue = "15") int pageSize) {
         Pageable pageable = PageRequest.of(page - 1, pageSize, Sort.by(Sort.Direction.DESC, "id"));
-        return orderService.getOrders(memberId, pageable);
+        return orderService.getOrders(user.id(), pageable);
     }
     
     // 주문 상세 단건 조회
     @Override
     @MemberOnly
     @GetMapping("/details/{orderDetailId}")
-    public OrderDetailResponse getOderDetail(@PathVariable Long orderDetailId){
-        return orderService.getOderDetail(orderDetailId);
+    public OrderDetailResponse getOderDetail(@Auth User user, @PathVariable Long orderDetailId){
+        return orderService.getOderDetail(user.id(), orderDetailId);
     }
     
     // 주문 상세 전체 조회
     @Override
     @MemberOnly
     @GetMapping("/{orderId}/details")
-    public List<OrderDetailResponse> getOrderDetails(@PathVariable Long orderId) {
-        return orderService.getOrderDetails(orderId);
+    public List<OrderDetailResponse> getOrderDetails(@Auth User user, @PathVariable Long orderId) {
+        return orderService.getOrderDetails(user.id(), orderId);
     }
 
     // 주문 전체 취소
     @Override
     @MemberOnly
     @PatchMapping("/{orderId}")
-    public IdResponse cancelOrder(@PathVariable Long orderId) {
-        return orderService.cancelOrder(orderId);
+    public IdResponse cancelOrder(@Auth User user, @PathVariable Long orderId) {
+        return orderService.cancelOrder(user.id(), orderId);
     }
 
     // 주문 부분 취소
     @Override
     @MemberOnly
     @PatchMapping("/details/{orderDetailId}")
-    public IdResponse cancelOrderDetail(@PathVariable Long orderDetailId) {
-        return orderService.cancelOrderDetail(orderDetailId);
+    public IdResponse cancelOrderDetail(@Auth User user, @PathVariable Long orderDetailId) {
+        return orderService.cancelOrderDetail(user.id(), orderDetailId);
     }
 }
