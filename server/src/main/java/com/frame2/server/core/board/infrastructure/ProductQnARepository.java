937e1cea8bbad2e09e19e92cb418d866aa782436
jaho96 <jaho321@naver.com>
2025-01-24 03:33:37 +0900

    refactor: 상품 QnA repository JPQL을 spring data jpa repository 명명 규칙으로 변경

--- server/src/main/java/com/frame2/server/core/board/infrastructure/ProductQnARepository.java
+++ server/src/main/java/com/frame2/server/core/board/infrastructure/ProductQnARepository.java
@@ -4,24 +4,18 @@ import com.frame2.server.core.board.domain.ProductQnA;
 import com.frame2.server.core.support.exception.DomainException;
 import com.frame2.server.core.support.exception.ExceptionType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductQnARepository extends JpaRepository<ProductQnA, Long> {
 
-    default ProductQnA findOne(Long id) {
-        return findById(id)
+    default ProductQnA findOne(Long productId, Long productQnAId) {
+        return findByProductIdAndIdAndDeleteStatusIsFalse(productId, productQnAId)
                 .orElseThrow(() -> new DomainException(ExceptionType.PRODUCT_QNA_NOT_FOUND));
     }
 
-    @Override
-    @Query("select prQnA from ProductQnA prQnA where prQnA.id =:id and prQnA.deleteStatus = false ")
-    Optional<ProductQnA> findById(@Param("id") Long id);
+    Optional<ProductQnA> findByProductIdAndIdAndDeleteStatusIsFalse(Long productId, Long productQnAId);
 
-    @Override
-    @Query("select prQnA from ProductQnA prQnA where prQnA.deleteStatus = false ")
-    List<ProductQnA> findAll();
+    List<ProductQnA> findByProductIdAndDeleteStatusFalse(Long productId);
 }
