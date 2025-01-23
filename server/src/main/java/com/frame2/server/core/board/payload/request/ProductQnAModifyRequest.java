c491d8bdc3ac0f837a7b32cc37f88555ce23c6cb
jaho96 <jaho321@naver.com>
2025-01-23 18:15:58 +0900

    refactor: requestDto 불필요한 매개변수 제거 및 변경

--- server/src/main/java/com/frame2/server/core/board/payload/request/ProductQnAModifyRequest.java
+++ server/src/main/java/com/frame2/server/core/board/payload/request/ProductQnAModifyRequest.java
@@ -1,10 +1,8 @@
 package com.frame2.server.core.board.payload.request;
 
 import com.frame2.server.core.board.domain.ProductQnA;
-import com.frame2.server.core.member.domain.Member;
 
 public record ProductQnAModifyRequest(
-        Long id,
         String title,
         String question
 ) {
