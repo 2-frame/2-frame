8e7924aa919b682009c0e0796b7e52b4f942f210
jaho96 <jaho321@naver.com>
2025-01-23 18:17:40 +0900

    refactor: 상품 QnA repository 삭제 여부에 따른 검색 구현

--- server/src/main/java/com/frame2/server/core/board/infrastructure/ProductQnARepository.java
+++ server/src/main/java/com/frame2/server/core/board/infrastructure/ProductQnARepository.java
@@ -3,12 +3,25 @@ package com.frame2.server.core.board.infrastructure;
 import com.frame2.server.core.board.domain.ProductQnA;
 import com.frame2.server.core.support.exception.DomainException;
 import com.frame2.server.core.support.exception.ExceptionType;
-import lombok.Builder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductQnARepository extends JpaRepository<ProductQnA, Long> {
-    default ProductQnA findbyidProductQnA(Long id){
+
+    default ProductQnA findOne(Long id) {
         return findById(id)
                 .orElseThrow(() -> new DomainException(ExceptionType.PRODUCT_QNA_NOT_FOUND));
     }
+
+    @Override
+    @Query("select prQnA from ProductQnA prQnA where prQnA.id =:id and prQnA.deleteStatus = false ")
+    Optional<ProductQnA> findById(@Param("id") Long id);
+
+    @Override
+    @Query("select prQnA from ProductQnA prQnA where prQnA.deleteStatus = false ")
+    List<ProductQnA> findAll();
 }
