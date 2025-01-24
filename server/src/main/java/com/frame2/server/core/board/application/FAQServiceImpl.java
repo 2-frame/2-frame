31f184955818b033f321590efd5f62942a46a969
jaho96 <jaho321@naver.com>
2025-01-24 11:46:01 +0900

    refactor: faq service  수정

--- server/src/main/java/com/frame2/server/core/board/application/FAQServiceImpl.java
+++ server/src/main/java/com/frame2/server/core/board/application/FAQServiceImpl.java
@@ -1,6 +1,7 @@
 package com.frame2.server.core.board.application;
 
 import com.frame2.server.core.board.domain.FAQ;
+import com.frame2.server.core.board.domain.FAQCategory;
 import com.frame2.server.core.board.infrastructure.FAQRepository;
 import com.frame2.server.core.board.payload.response.FAQResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +25,15 @@ public class FAQServiceImpl implements FAQService {
 
     @Transactional(readOnly = true)
     public List<FAQResponse> getFAQList() {
-        return faqRepository.findAll()
+        return faqRepository.findAllByDeleteStatusFalse()
+                .stream()
+                .map(FAQResponse::from)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<FAQResponse> getCategoryFAQList(FAQCategory category) {
+        return faqRepository.findByCategoryAndDeleteStatusFalse(category)
                 .stream()
                 .map(FAQResponse::from)
                 .toList();
