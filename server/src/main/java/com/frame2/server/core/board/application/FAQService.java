472ce98c32889e45f81f8f9c9adbe668409a9ad1
jaho96 <jaho321@naver.com>
2025-01-24 11:45:53 +0900

    refactor: faq service  수정

--- server/src/main/java/com/frame2/server/core/board/application/FAQService.java
+++ server/src/main/java/com/frame2/server/core/board/application/FAQService.java
@@ -1,5 +1,6 @@
 package com.frame2.server.core.board.application;
 
+import com.frame2.server.core.board.domain.FAQCategory;
 import com.frame2.server.core.board.payload.response.FAQResponse;
 
 import java.util.List;
@@ -7,7 +8,10 @@ import java.util.List;
 public interface FAQService {
 
     //단건 조회
-    public FAQResponse getFAQ(Long faqId);
+    FAQResponse getFAQ(Long faqId);
 
-    public List<FAQResponse> getFAQList();
+    List<FAQResponse> getFAQList();
+
+    // 카테고리에 따른 전체 조회
+    List<FAQResponse> getCategoryFAQList(FAQCategory category);
 }
