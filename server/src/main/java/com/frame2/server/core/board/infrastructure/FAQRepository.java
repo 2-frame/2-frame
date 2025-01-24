561ec05d2a4db2433c99b87675e7bd5c1c824ac9
jaho96 <jaho321@naver.com>
2025-01-24 11:45:43 +0900

    refactor: faq repository  수정

--- server/src/main/java/com/frame2/server/core/board/infrastructure/FAQRepository.java
+++ server/src/main/java/com/frame2/server/core/board/infrastructure/FAQRepository.java
@@ -1,10 +1,10 @@
 package com.frame2.server.core.board.infrastructure;
 
 import com.frame2.server.core.board.domain.FAQ;
+import com.frame2.server.core.board.domain.FAQCategory;
 import com.frame2.server.core.support.exception.DomainException;
 import com.frame2.server.core.support.exception.ExceptionType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,14 +12,12 @@ import java.util.Optional;
 public interface FAQRepository extends JpaRepository<FAQ, Long> {
 
     default FAQ findOne(Long faqId) {
-        return findById(faqId).orElseThrow(() -> new DomainException(ExceptionType.FAQ_NOT_FOUND));
+        return findByIdAndDeleteStatusFalse(faqId).orElseThrow(() -> new DomainException(ExceptionType.FAQ_NOT_FOUND));
     }
 
-    @Override
-    @Query("select faq from FAQ faq where faq.id =:faqId and faq.deleteStatus = false ")
-    Optional<FAQ> findById(Long faqId);
+    Optional<FAQ> findByIdAndDeleteStatusFalse(Long id);
 
-    @Override
-    @Query("select faq from FAQ faq where faq.deleteStatus = false ")
-    List<FAQ> findAll();
+    List<FAQ> findAllByDeleteStatusFalse();
+
+    List<FAQ> findByCategoryAndDeleteStatusFalse(FAQCategory category);
 }
