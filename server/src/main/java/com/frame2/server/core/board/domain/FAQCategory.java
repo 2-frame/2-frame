c81bb9f13f28299de70acd2e2e349437f55a4540
jaho96 <jaho321@naver.com>
2025-01-24 11:45:14 +0900

    refactor: faq FAQCategory eum 수정

--- server/src/main/java/com/frame2/server/core/board/domain/FAQCategory.java
+++ server/src/main/java/com/frame2/server/core/board/domain/FAQCategory.java
@@ -2,7 +2,19 @@ package com.frame2.server.core.board.domain;
 
 public enum FAQCategory {
 
+    // 취소/교환/반품
     CANCELLATION_EXCHANGE_RETURN,
+
+    // 배송 문의
     DELIVERY_INQUIRY,
+
+    // 회원 서비스
+    MEMBER_SERVICE,
+
+    // 주문/결제
+    ORDER_PAYMENT,
+
+    // 환불
+    REFUND
     
 }
